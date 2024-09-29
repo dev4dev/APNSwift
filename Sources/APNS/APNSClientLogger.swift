68e79202f8589b9c10b0e3661936783f53f90c5b
Alex Antonyuk <alex@antonyuk.me>
2024-09-29 22:25:08 +0300

    ok

--- Sources/APNS/APNSClientLogger.swift
+++ Sources/APNS/APNSClientLogger.swift
@@ -5,26 +5,31 @@
 //  Created by Alex Antonyuk on 04.05.2024.
 //
 
-import Foundation
 import AsyncHTTPClient
+import Foundation
+import os
 
-public protocol APNSClientLogger {
+public protocol APNSClientLogger: Sendable {
     func logTransaction(request: HTTPClientRequest, response: HTTPClientResponse)
 }
 
 public final class APNSClientMemoryLogger: APNSClientLogger {
 
-    public private(set) var logs: [(request: HTTPClientRequest, response: HTTPClientResponse)] = []
+    public let logs: OSAllocatedUnfairLock<[(request: HTTPClientRequest, response: HTTPClientResponse)]> = .init(initialState: [])
 
     public init() {
 
     }
 
     public func logTransaction(request: HTTPClientRequest, response: HTTPClientResponse) {
-        logs.append((request: request, response: response))
+        logs.withLock {
+            $0.append((request: request, response: response))
+        }
     }
 
     public func pop() -> (request: HTTPClientRequest, response: HTTPClientResponse)? {
-        logs.popLast()
+        logs.withLock {
+            $0.popLast()
+        }
     }
 }
